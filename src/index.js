e061037ed1f6e26bf30f4c2e9a5adf00ebf4f34f
Polina <polinastuff@gmail.com>
2022-12-25 22:53:26 +0200

    Created files and folders from homework 4

---
+++ src/index.js
@@ -0,0 +1,90 @@
+//to get the date
+function getDate() {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let date = new Date();
+  let day = date.getDay();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let fullDate = `${days[day]}, ${hours}:${minutes}`;
+
+  document.querySelector("#full-date").innerHTML = fullDate;
+}
+
+getDate();
+
+//to change the city and data
+
+function changeData(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML =
+    Math.round(response.data.main.temp) + "° C";
+  document.querySelector("#feels-like-temperature").innerHTML =
+    Math.round(response.data.main.feels_like) + "°";
+  document.querySelector("#main-description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
+
+function updateCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-bar").value;
+  getCityData(city);
+}
+
+function getCityData(city) {
+  let apiKey = "d9682284ab68bba42fdaf690958f42f3";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(changeData);
+  getDate();
+}
+
+let searchForm = document.querySelector("#city-form");
+searchForm.addEventListener("submit", updateCity);
+
+getCityData("Berlin");
+
+//get current location data
+
+function getURL(location) {
+  let apiKey = "d9682284ab68bba42fdaf690958f42f3";
+  let URL = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${apiKey}&units=metric`;
+  return URL;
+}
+
+function getLocation(response) {
+  let latitude = response.coords.latitude;
+  let longitude = response.coords.longitude;
+  let location = `lat=${latitude}&lon=${longitude}`;
+
+  axios.get(getURL(location)).then(changeData);
+}
+
+function getCurrentLocationData(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocation);
+  getDate();
+}
+
+let getCurrentLocationButton = document.querySelector(
+  "#current-location-button"
+);
+getCurrentLocationButton.addEventListener("click", getCurrentLocationData);
