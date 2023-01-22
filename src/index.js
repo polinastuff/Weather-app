d200f2c898b91c56cbf67f96c338ad448df409b9
Polina <polinastuff@gmail.com>
2023-01-22 14:18:10 +0200

    fixed not secure link

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ function changeData(response) {
     .querySelector("#weather-icon")
     .setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
   getForecast(response.data.coord);
 }
